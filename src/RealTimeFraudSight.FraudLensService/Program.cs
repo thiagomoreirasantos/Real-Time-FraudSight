f4596e93a548aad7937da5843c77110553099965
thiagomoreirasantos <tmoreiradossantos733@gmail.com>
2023-12-01 16:45:28 +0000

    Create tge first projects Create .gitignore files

---
+++ src/RealTimeFraudSight.FraudLensService/Program.cs
@@ -0,0 +1,10 @@
+using RealTimeFraudSight.FraudLensService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
