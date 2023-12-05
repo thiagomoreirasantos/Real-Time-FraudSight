e01202e34c70aea4c6dcebd5bb5a41d9f9da24db
thiagomoreirasantos <tmoreiradossantos733@gmail.com>
2023-12-05 23:43:46 +0000

    Add methods to Order class

--- src/RealTimeFraudSight.Domain/Order.cs
+++ src/RealTimeFraudSight.Domain/Order.cs
@@ -6,7 +6,7 @@ namespace RealTimeFraudSight.Domain
         public Guid CustomerId { get; set; }
         public DateTime OrderDate { get; set; }
         public required IList<InventoryItem> Items { get; set; }
-        public int TotalAmount { get; set; }    
+        public decimal TotalAmount { get; set; }    
         public required string ShippingAddress { get; set; }
         public required string BillingAddress { get; set; }
         public int PaymentMethod { get; set; }
@@ -14,5 +14,48 @@ namespace RealTimeFraudSight.Domain
         public int ShippingMethod { get; set; }
         public required string TrackingNumber { get; set; }
         public required string CustomerNotes { get; set; }
+
+        public bool CheckStatus()
+        {
+            return this.OrderStatus == 1;
+        }
+
+        public bool CheckPayment()
+        {
+            return this.PaymentMethod == 1;
+        }
+
+        public decimal CalculateTotal()
+        {
+            return this.Items.Sum(x => x.Price);
+        }        
+
+        public void AddItem(InventoryItem item)
+        {
+            this.Items.Add(item);
+        }   
+
+        public void RemoveItem(InventoryItem item)
+        {
+            this.Items.Remove(item);
+        }
+
+        public void UpdateOrder(DateTime orderDate, decimal totalAmount, string shippingAddress, string billingAddress, int paymentMethod, int orderStatus, int shippingMethod, string trackingNumber, string customerNotes)
+        {
+            this.OrderDate = orderDate;
+            this.TotalAmount = totalAmount;
+            this.ShippingAddress = shippingAddress;
+            this.BillingAddress = billingAddress;
+            this.PaymentMethod = paymentMethod;
+            this.OrderStatus = orderStatus;
+            this.ShippingMethod = shippingMethod;
+            this.TrackingNumber = trackingNumber;
+            this.CustomerNotes = customerNotes;
+        }           
+
+        public void CancelOrder()
+        {
+            this.OrderStatus = 2;
+        }
     }
 }
