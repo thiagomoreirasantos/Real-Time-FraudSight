17356a96fff316c65d446fefd30df9a7d405ebfa
thiagomoreirasantos <tmoreiradossantos733@gmail.com>
2023-12-05 23:22:12 +0000

    Fill class FlowKeeper

--- src/RealTimeFraudSight.FraudLensService/Flowkeeper.cs
+++ src/RealTimeFraudSight.FraudLensService/Flowkeeper.cs
@@ -16,54 +16,51 @@ namespace RealTimeFraudSight.FraudLensService
         {
             _transformBlock = new TransformBlock<FlowContext, FlowContext>(new Func<FlowContext, Task<FlowContext>>(this.ExecutePipeline));
         }
-        
+
         public Task Completion => this._transformBlock.Completion;
 
         protected abstract Task<FlowContext> ExecutePipeline(FlowContext context);
 
-        public void Complete()
-        {
-            throw new NotImplementedException();
-        }
+        public void Complete() => this._transformBlock.Complete();
 
         public FlowContext? ConsumeMessage(DataflowMessageHeader messageHeader, ITargetBlock<FlowContext> target, out bool messageConsumed)
         {
-            throw new NotImplementedException();
+            return ((ISourceBlock<FlowContext>)this._transformBlock).ConsumeMessage(messageHeader, target, out messageConsumed);
         }
 
         public void Fault(Exception exception)
         {
-            throw new NotImplementedException();
+            ((IDataflowBlock) this._transformBlock).Fault(exception);
         }
 
         public IDisposable LinkTo(ITargetBlock<FlowContext> target, DataflowLinkOptions linkOptions)
         {
-            throw new NotImplementedException();
+            return this._transformBlock.LinkTo(target, linkOptions);
         }
 
         public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, FlowContext messageValue, ISourceBlock<FlowContext>? source, bool consumeToAccept)
         {
-            throw new NotImplementedException();
+            return ((ITargetBlock<FlowContext>)this._transformBlock).OfferMessage(messageHeader, messageValue, source, consumeToAccept);
         }
 
         public void ReleaseReservation(DataflowMessageHeader messageHeader, ITargetBlock<FlowContext> target)
         {
-            throw new NotImplementedException();
+            ((ISourceBlock<FlowContext>)this._transformBlock).ReleaseReservation(messageHeader, target);
         }
 
         public bool ReserveMessage(DataflowMessageHeader messageHeader, ITargetBlock<FlowContext> target)
         {
-            throw new NotImplementedException();
+            return ((ISourceBlock<FlowContext>)this._transformBlock).ReserveMessage(messageHeader, target);
         }
 
         public bool TryReceive(Predicate<FlowContext>? filter, [MaybeNullWhen(false)] out FlowContext item)
         {
-            throw new NotImplementedException();
+            return this._transformBlock.TryReceive(filter, out item);
         }
 
         public bool TryReceiveAll([NotNullWhen(true)] out IList<FlowContext>? items)
         {
-            throw new NotImplementedException();
+            return this._transformBlock.TryReceiveAll(out items);
         }
     }
 }
